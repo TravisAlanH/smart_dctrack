6785e4a9beb3551cc895ec429b295bfdae3fe01c
Travis Heidelberger <70112002+TravisAlanH@users.noreply.github.com>
2025-11-20 09:06:41 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://localhost:5173"],
+    origin: ["http://localhost:5173", "https://localhost:5173", "https://audittool-frontend.onrender.com"],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "x-dctrack-host"],
   })
